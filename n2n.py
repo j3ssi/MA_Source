d7748f36c89d9bac7953e61df88a6845ebad194b
j3ssi <jessica.buehler@udo.edu>
2020-01-01 20:44:20 +0100

    pT +N2N

--- n2n.py
+++ n2n.py
@@ -40,7 +40,7 @@ class N2N(nn.Module):
 
         # 5
         self.avgpool = nn.AvgPool2d(4)
-        self.fc = nn.Linear(128, num_classes)
+        self.fc = nn.Linear(1024, num_classes)
         self.relu = nn.ReLU(inplace=True)
 
         for m in self.modules():
