d952056e6aa4f66978175466247614291d244b1c
j3ssi <jessica.buehler@udo.edu>
2020-01-01 15:48:56 +0100

    pT +N2N

--- main.py
+++ main.py
@@ -325,8 +325,8 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         if use_cuda:
             inputs, targets = inputs.cuda(), targets.cuda()
 
-        with torch.no_grad():
-            inputs = Variable(inputs)
+
+        inputs = Variable(inputs)
         target = torch.autograd.Variable(targets)
 
 #       inputs, targets = torch.autograd.Variable(inputs, volatile=True),\
