bddf13fb98f956b2edca5471fa633fe4b91d8257
j3ssi <jessica.buehler@udo.edu>
2020-01-01 20:11:16 +0100

    pT +N2N

--- n2n.py
+++ n2n.py
@@ -39,7 +39,7 @@ class N2N(nn.Module):
         self.bn9 = nn.BatchNorm2d(16)
 
         # 5
-        self.avgpool = nn.AdaptiveAvgPool2d(16)
+        self.avgpool = nn.AdaptiveAvgPool2d(10)
         self.fc = nn.Linear(1, num_classes)
         self.relu = nn.ReLU(inplace=True)
 
