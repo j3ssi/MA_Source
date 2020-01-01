7fe3fe3ffb2a09b49669dd2a4253b84e69a458ee
j3ssi <jessica.buehler@udo.edu>
2020-01-01 14:38:52 +0100

    pT +N2N

--- src/src/custom/group_lasso_regs.py
+++ src/src/custom/group_lasso_regs.py
@@ -33,8 +33,8 @@ def get_group_lasso_global(model, arch):
         # Lasso added to only the neuronal layers
         if ('weight' in name) and any([i for i in ['conv', 'fc'] if i in name]):
             if param.dim() == 4:
-                print("\nName:")
-                print("\n", name.split('.')[1])
+                print("\nName:\n")
+                print( name.split('.')[1])
                 conv_dw = int(name.split('.')[1].split('conv')[1]) %2 == 0
                 add_lasso = ('mobilenet' not in arch) or ('mobilenet' in arch and not conv_dw)
 
