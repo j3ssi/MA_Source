1619f10d78e42681a632403dd0a919cf6816bf17
j3ssi <jessica.buehler@udo.edu>
2020-01-01 16:14:53 +0100

    pT +N2N

--- n2n.py
+++ n2n.py
@@ -71,7 +71,7 @@ class N2N(nn.Module):
             #print(self.__dict__)
 
 
-            names = list(self.named_modules())
+            names = self.named_modules()
             print(names)
             if(convStr not in names ):
                 # Forward at last layer
