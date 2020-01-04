e9bc26457daa32e1f1c813ec9933941395c06bb8
j3ssi <jessica.buehler@udo.edu>
2020-01-04 23:19:28 +0100

    n2n + pruneTrain

--- n2n.py
+++ n2n.py
@@ -131,11 +131,7 @@ class N2N(nn.Module):
             bn2 = copy.deepcopy(bn)
             modelList.insert(j+3, bn2)
             newModel = nn.Sequential(*modelList)
-            for name, module in newModel.named_children():
-                print("\nName:")
-                print(name)
-                print("\n")
-                print(module)
+            print(self.__dict__())
             return newModel
             #     if posStr in name:
             #         i = name.index(posStr)
