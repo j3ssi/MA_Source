16b586090deb316e5503cd34947eb34bf536e860
j3ssi <jessica.buehler@udo.edu>
2020-01-05 02:18:17 +0100

    n2n + pruneTrain

--- n2n.py
+++ n2n.py
@@ -44,8 +44,8 @@ class N2N(nn.Module):
         self.bn9 = nn.BatchNorm2d(16)
 
         # 5
-        self.avgpool = nn.AdaptiveAvgPool2d((None,128))
-        self.fc = nn.Linear(128, num_classes)
+        self.avgpool = nn.AdaptiveAvgPool2d((512,512))
+        self.fc = nn.Linear(512, num_classes)
         self.relu = nn.ReLU(inplace=True)
 
         for m in self.modules():
