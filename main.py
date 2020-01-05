53d5d777661e5337d967044aa4f14867f7a5accb
j3ssi <jessica.buehler@udo.edu>
2020-01-05 22:25:24 +0100

    pt + n2n

--- main.py
+++ main.py
@@ -227,7 +227,7 @@ def main():
         return
 
     # Train and val
-    for epochNet2Net in range(1, 5):
+    for epochNet2Net in range(1, 2):
         print("\n Net 2 Net Durchläufe:")
         print(epochNet2Net)
 
