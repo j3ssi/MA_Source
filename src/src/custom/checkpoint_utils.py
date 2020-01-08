5a45405305029df0cfafce5804ff72dec687ce5b
j3ssi <jessica.buehler@udo.edu>
2020-01-08 10:49:07 +0100

    final!

--- src/src/custom/checkpoint_utils.py
+++ src/src/custom/checkpoint_utils.py
@@ -482,7 +482,7 @@ def _genDenseModel(model, dense_chs, optimizer, arch, dataset):
         print(name)
         if 'running_mean' in name or 'running_var' in name:
             i = int(name.split('.')[1])
-            w_name = 'module.conv' +str(int((i+1)/2)) + 'weight'
+            w_name = 'module.conv' +str(int((i+1)/2)) + '.weight'
             dense_out_ch_idxs = dense_chs[w_name]['out_chs']
             num_out_ch = len(dense_out_ch_idxs)
             new_buf = Parameter(torch.Tensor(num_out_ch)).cuda()
