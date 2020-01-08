f64d783580a77272e03ec5f88047e50f687fd2a5
j3ssi <jessica.buehler@udo.edu>
2020-01-08 06:41:03 +0100

    PT + N2N

--- src/src/custom/checkpoint_utils.py
+++ src/src/custom/checkpoint_utils.py
@@ -220,15 +220,11 @@ def _makeSparse(model, threshold, threshold_type, dataset, is_gating=False, reco
             if param.dim() == 4:
                 for c in range(dims[1]):
                     if param[:, c, :, :].abs().max() > 0:
-                        print("\n>dense in:")
-                        print(name)
                         dense_in_chs.append(c)
 
                 # Forcing sparse output channels to zero
                 for c in range(dims[0]):
                     if param[c, :, :, :].abs().max() > 0:
-                        print("\n>dense out:")
-                        print(name)
                         dense_out_chs.append(c)
 
             # Forcing input channels of FC layer to zero
@@ -416,6 +412,9 @@ def _genDenseModel(model, dense_chs, optimizer, arch, dataset):
 
         # Change parameters of non-neural computing layers (BN, biases)
         elif not isinstance(model.module_list[i], nn.AdaptiveAvgPool2d):
+
+
+
             w_name = name.replace('bias', 'weight').replace('bn', 'conv')
             print("\n> w_name: ")
             print(w_name)
