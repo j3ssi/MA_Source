32369a543772396b3110a26d4923f88f9f4f3e19
j3ssi <jessica.buehler@udo.edu>
2020-01-08 04:08:24 +0100

    PT + N2N

--- src/src/models/cifar/resnet32_flat.py
+++ src/src/models/cifar/resnet32_flat.py
@@ -115,13 +115,13 @@ class ResNet32(nn.Module):
         self.fc = nn.Linear(64, num_classes)
         self.relu = nn.ReLU(inplace=True)
 
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-                m.weight.data.normal_(0, math.sqrt(2. / n))
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
-                m.bias.data.zero_()
+        # for m in self.modules():
+        #     if isinstance(m, nn.Conv2d):
+        #         n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+        #         m.weight.data.normal_(0, math.sqrt(2. / n))
+        #     elif isinstance(m, nn.BatchNorm2d):
+        #         m.weight.data.fill_(1)
+        #         m.bias.data.zero_()
 
     # This part of architecture remains the same
     def forward(self, x):
