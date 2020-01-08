9ec85e87ca9b6246d42c8526ab19cbdc700ca13b
j3ssi <jessica.buehler@udo.edu>
2020-01-08 09:06:08 +0100

    PT + N2N

--- src/src/custom/checkpoint_utils.py
+++ src/src/custom/checkpoint_utils.py
@@ -202,13 +202,15 @@ def _makeSparse(model, threshold, threshold_type, arch, dataset, is_gating=False
     dense_chs, chs_temp, idx = {}, {}, 0
     altList = []
     for name, param in model.named_parameters():
+        print("\n\n>i")
+        print(i)
         i = int(name.split('.')[1])
         if i % 2 == 0:
             altList.append('module.conv' + str(i/2) + '.weight')
         if i % 2 == 1 and 'weight' in name:
-            altList.append('module.bn' + str(i/2-1) + ".weight")
+            altList.append('module.bn' + str((i-1)/2) + ".weight")
         if i % 2 == 1 and 'bias' in name:
-            altList.append('module.bn' + str(i/2-1) + ".bias")
+            altList.append('module.bn' + str((i-1)/2) + ".bias")
 
     altList[-2].replace('bn', 'fc')
     altList[-1].replace('bn', 'fc')
@@ -364,15 +366,15 @@ def _genDenseModel(model, dense_chs, optimizer, arch, dataset):
     rm_list = []
     altList = []
     for name, param in model.named_parameters():
-        print("\n >name:")
-        print(name)
+        print("\n\n>i")
+        print(i)
         i = int(name.split('.')[1])
         if i % 2 == 0:
             altList.append('module.conv' + str(i/2) + '.weight')
         if i % 2 == 1 and 'weight' in name:
-            altList.append('module.bn' + str(i/2-1) + ".weight")
+            altList.append('module.bn' + str((i-1)/2) + ".weight")
         if i % 2 == 1 and 'bias' in name:
-            altList.append('module.bn' + str(i/2-1) + ".bias")
+            altList.append('module.bn' + str((i-1)/2) + ".bias")
 
     altList[-2].replace('bn', 'fc')
     altList[-1].replace('bn', 'fc')
