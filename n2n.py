532cce79ad16e756d42aa2dfd747851f689aaba1
j3ssi <jessica.buehler@udo.edu>
2020-01-08 03:01:19 +0100

    PT + N2N

--- n2n.py
+++ n2n.py
@@ -150,8 +150,8 @@ class N2N(nn.Module):
             conv = model.module_list[pos*2-2]
             bn = model.module_list[pos*2-1]
             conv2 = copy.deepcopy(conv)
-            noise = torch.Tensor(conv2.shape())
-            noise = torch.rand(0,0.5)
+            noise = torch.Tensor(conv2.shape()).random_(0,0.3)
+            #noise = torch.rand(0,0.5)
             conv2.weight.data += noise
             bn2 = copy.deepcopy(bn)
             model.module_list.insert(pos*2+2, conv2)
