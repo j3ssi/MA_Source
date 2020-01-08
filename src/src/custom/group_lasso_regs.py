9a55def28082884e329ea76f7365aaf975785bfd
j3ssi <jessica.buehler@udo.edu>
2020-01-08 11:08:37 +0100

    final!

--- src/src/custom/group_lasso_regs.py
+++ src/src/custom/group_lasso_regs.py
@@ -28,7 +28,7 @@ import torch.nn as nn
 """
 
 
-def get_group_lasso_global(model, arch):
+def get_group_lasso_global(model):
     lasso_in_ch = []
     lasso_out_ch = []
     altList = []
@@ -57,8 +57,7 @@ def get_group_lasso_global(model, arch):
         if ('weight' in name) and any([i for i in ['conv', 'fc'] if i in name]):
             if param.dim() == 4:
                 conv_dw = int(name.split('.')[1].split('conv')[1]) %2 == 0
-                add_lasso = ('mobilenet' not in arch) or ('mobilenet' in arch and not conv_dw)
-
+                add_lasso = True
                 # Exclude depth-wise convolution layers from regularization
                 if add_lasso:
                     if 'conv1.' not in name:
