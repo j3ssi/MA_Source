333c7acdd386098e141aecd06e7d121ced1fd2cc
j3ssi <jessica.buehler@udo.edu>
2020-01-08 04:26:21 +0100

    PT + N2N

--- main.py
+++ main.py
@@ -53,8 +53,8 @@ parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
                     help='number of data loading workers (default: 4)')
 parser.add_argument('--epochs', default=300, type=int, metavar='N',
                     help='number of total epochs to run')
-#parser.add_argument('--start-epoch', default=1, type=int, metavar='N',
-#                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--start-epoch', default=1, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
 parser.add_argument('--train_batch', default=128, type=int, metavar='N',
                     help='train batchsize')
 parser.add_argument('--test_batch', default=100, type=int, metavar='N',
