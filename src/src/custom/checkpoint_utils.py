8c9116cca9f3f8115030a75b8d8bc421bb6ba0e6
j3ssi <jessica.buehler@udo.edu>
2020-01-13 00:48:12 +0100

    rm layers anpassen

--- src/src/custom/checkpoint_utils.py
+++ src/src/custom/checkpoint_utils.py
@@ -459,9 +459,9 @@ def _genDenseModel(model, dense_chs, optimizer, dataset):
 
     # Sanity check => Print out optimizer parameters after change
     print ("[INFO] ==== Size of parameter group (After)")
-        for g in optimizer.param_groups:
-            for idx, g2 in enumerate(g['params']):
-                print("idx:{}, param_shape:{}".format(idx, list(g2.shape)))
+    for g in optimizer.param_groups:
+        for idx, g2 in enumerate(g['params']):
+            print("idx:{}, param_shape:{}".format(idx, list(g2.shape)))
 
 # Sanity check => Check the changed parameters
 # for name, param in model.named_parameters():
