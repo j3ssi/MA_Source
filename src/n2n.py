17d2db8d1ba7e0c1facdfea0089a2bdb6e28e79b
j3ssi <jessica.buehler@udo.edu>
2020-01-14 15:11:08 +0100

    Final?

--- src/n2n.py
+++ src/n2n.py
@@ -104,7 +104,7 @@ class N2N(nn.Module):
             module_list1.append(fc)
             self.module_list = module_list1
             self.relu = nn.ReLU(inplace=True)
-
+            print("\nnew Model: ", self)
 
     def forward(self, x):
         odd = False
