ef1f6169d24ce1ca60a2d0add4fca985db18552c
j3ssi <jessica.buehler@udo.edu>
2020-01-15 12:25:08 +0100

    messungen

--- src/n2n.py
+++ src/n2n.py
@@ -74,10 +74,10 @@ class N2N(nn.Module):
             # print("\naltList", altList)
             module_list1 = nn.ModuleList()
             for i in range(len(altList)):
-                print("\n>i: ", i)
+                #print("\n>i: ", i)
                 name = altList[i]
                 param = paramList[i]
-                print("\nName: ", name)
+                #print("\nName: ", name)
                 if 'conv' in name:
                     dims = list(param.shape)
                     in_chs = dims[1]
@@ -125,8 +125,8 @@ class N2N(nn.Module):
         odd = False
         first = True
         bn = False
-        _x = None
-        printNet = True
+        #_x = None
+        printNet = False
         i = 0
         for module in self.module_list:
             if isinstance(module, nn.AdaptiveAvgPool2d):
