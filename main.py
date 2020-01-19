a75c01c1370d160bee237228679d1d0401a29319
j3ssi <jessica.buehler@udo.edu>
2020-01-19 21:13:21 +0100

    ResNet Stages for dynamic model

--- main.py
+++ main.py
@@ -56,7 +56,7 @@ parser.add_argument('--weight-decay', '--wd', default=5e-4, type=float,
                     metavar='W', help='weight decay (default: 1e-4)')
 parser.add_argument('--manualSeed', type=int, help='manual seed')
 parser.add_argument('--gpu_id', default='2', type=str, help='id(s) for CUDA_VISIBLE_DEVICES')
-parser.add_argument('-s', '--numOfStages', default=3, type=int, help='defines the number of stages in the network')
+parser.add_argument('-s', '--numOfStages', default=1, type=int, help='defines the number of stages in the network')
 parser.add_argument('-n', '--numOfBlocksinStage', type =int, default=2, help='defines the number of Blocks per Stage' )
 parser.add_argument('-l', '--layersInBlock', type = int, default =3, help='defines the number of')
 # PruneTrain
