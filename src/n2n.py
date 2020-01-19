38e730af2e8daaf1f39773f87682a2001e29be53
j3ssi <jessica.buehler@udo.edu>
2020-01-19 18:02:24 +0100

    ResNet Stages for dynamic model

--- src/n2n.py
+++ src/n2n.py
@@ -260,17 +260,17 @@ class N2N(nn.Module):
             for block in range(0, self.numOfBlocksinStage):
                 if first and stage == 0:
                     if i%2 == 1:
-                        stagesO[stage].append(n(i+2/2))
+                        stagesO[stage].append(n(int(i+2/2)))
                         i = i + 1
                     else:
-                        stagesI[stage].append(n(i+2/2))
+                        stagesI[stage].append(n(int(i+2/2)))
                         i = i + 1
                 elif first and stage > 0:
                     if i % 2 == 1:
-                        stagesO[stage].append(n(i+2/2))
+                        stagesO[stage].append(n(int(i+2/2)))
                         i = i + 1
                     else:
-                        stagesI[stage].append(n(i+2/2))
+                        stagesI[stage].append(n(int(i+2/2)))
                         i = i + 1
 
         print("\nStagesI: ", stagesI)
