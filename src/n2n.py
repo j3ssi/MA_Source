5042c9d9b1bc06286c69f1baf11a94c4a5dfe6b2
j3ssi <jessica.buehler@udo.edu>
2020-01-28 00:19:22 +0100

    stages

--- src/n2n.py
+++ src/n2n.py
@@ -297,16 +297,16 @@ class N2N(nn.Module):
                         j = j + 1
                         i = i + 1
                     elif (i-1) % self.layersInBlock == 1:
-                        stagesO[stage].append(n(int(j - 2 / 2)))
+                        stagesI[stage].append(n(int(j - 2 / 2)))
                         if printStages:
-                            print("\nstagesO: ", j)
+                            print("\nstagesI: ", j)
                         i = i + 1
                         j = j + 1
 
                     elif (i -1) % self.layersInBlock == 0:
-                        stagesI[stage].append(n(int(j - 2 / 2)))
+                        stagesO[stage].append(n(int(j - 2 / 2)))
                         if printStages:
-                            print("\nstagesI: ", j)
+                            print("\nstagesO: ", j)
                         i = i + 1
                         j = j + 1
                     else:
