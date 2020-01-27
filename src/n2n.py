fb9275086cb18afd1108d89195c58e4c36f018e4
j3ssi <jessica.buehler@udo.edu>
2020-01-27 21:54:12 +0100

    stages

--- src/n2n.py
+++ src/n2n.py
@@ -252,7 +252,7 @@ class N2N(nn.Module):
                 if printNet:
                     print("\nJ: ", j, " ; ", self.module_list[j])
                     print("\n\n X Shape 1: ", x.shape)
-                x = x.view(-1, self.sizeOfFC)
+                x = x.view(-1, 2*self.sizeOfFC)
                 print("\n\n X Shape 2: ", x.shape)
             except RuntimeError:
                 print("\n \n Oops!!!: ")
