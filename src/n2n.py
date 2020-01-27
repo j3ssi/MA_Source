ac127d9c092871a60aba5cfbb8ab683a38287c0a
j3ssi <jessica.buehler@udo.edu>
2020-01-27 22:05:11 +0100

    stages

--- src/n2n.py
+++ src/n2n.py
@@ -96,7 +96,7 @@ class N2N(nn.Module):
                         print("\nI:", i, " ; ", altList[-1])
                 else:
                     assert True, print("Hier fehlt noch was!!")
-            # print("\naltList", altList)
+            print("\naltList", altList)
             module_list = self.module_list
             for i in range(0, len(altList)):
                 # print("\n>i: ", i)
