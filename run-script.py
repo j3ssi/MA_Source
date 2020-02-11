7866caf1b023a81edaac43f48f9d5cb82155585f
j3ssi <jessica.buehler@udo.edu>
2020-02-11 01:38:32 +0100

    experimente

--- run-script.py
+++ run-script.py
@@ -7,13 +7,25 @@ def main():
     for i in s:
         for j in n:
             cmdLine = 'python3 main.py '
-            cmdLine = cmdLine + '--workers 4 --epochs 50 '
+            cmdLine = cmdLine + '--workers 4 --epochs 100 '
             cmdLine = cmdLine + '--learning-rate 0.1 --schedule 91 136 '
             cmdLine = cmdLine + '--gpu_id 2 --train_batch 128 '
-            cmdLine = cmdLine + '--en_group_lasso --test_batch 100 --sparse_interval 20'
+            cmdLine = cmdLine + '--en_group_lasso --test_batch 100 --sparse_interval 5'
             cmdLine = cmdLine + ' -s ' + str(i) + ' -n ' + str(j) +' -l 3'
             print (cmdLine)
             os.system(cmdLine)
+    for i in s:
+        for j in n:
+            print("\nohne PruneTrain")
+            cmdLine = 'python3 main.py '
+            cmdLine = cmdLine + '--workers 4 --epochs 100 '
+            cmdLine = cmdLine + '--learning-rate 0.1 --schedule 91 136 '
+            cmdLine = cmdLine + '--gpu_id 2 --train_batch 128 '
+            cmdLine = cmdLine + ' --test_batch 100 --sparse_interval 5'
+            cmdLine = cmdLine + ' -s ' + str(i) + ' -n ' + str(j) +' -l 3'
+            print (cmdLine)
+            os.system(cmdLine)
+
 
 if __name__ == '__main__':
     main()
