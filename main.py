bcfc59475fa626dda578097f6c9e0f1332bdc200
j3ssi <jessica.buehler@udo.edu>
2020-02-16 17:15:33 +0100

    trainings batch size

--- main.py
+++ main.py
@@ -106,7 +106,7 @@ state = {k: v for k, v in args._get_kwargs()}
 
 # GPU selection
 available, total = cuda.mem_get_info()
-print("Available: %f kB\nTotal:     %f kB"%(available, total))
+print("Available: %.2f kB\nTotal:     %f.2 kB"%(available/1e-3, total/1e-3))
 
 
 info = None
