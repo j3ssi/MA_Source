84f86e8f9dfc7af9c37e047dfa402fc8f1c42c23
j3ssi <jessica.buehler@udo.edu>
2020-02-17 13:55:00 +0100

    trainings batch

--- main.py
+++ main.py
@@ -369,7 +369,7 @@ def main():
     trainloader = data.DataLoader(trainset, batch_size=batch_size,
                                   shuffle=True, num_workers=args.workers)
 
-    torch.cuda.clear_memory_allocated()
+    torch.cuda.empty_cache()
     gc.collect()
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         if use_cuda:
