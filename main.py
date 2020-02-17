d48509c44744963d8f7585660f18e8f2cd7c4373
j3ssi <jessica.buehler@udo.edu>
2020-02-17 15:47:40 +0100

    trainings batch

--- main.py
+++ main.py
@@ -557,12 +557,12 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda, use_gpu, us
             if batch_idx % args.print_freq == 0:
                 print('Epoch: [{0}][{1}/{2}]\t'
                       'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                    'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                    'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                    'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                    'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                    epoch, batch_idx, len(trainloader), batch_time=batch_time,
-                data_time=data_time, loss=losses, top1=top1, top5=top5))
+                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                      epoch, batch_idx, len(trainloader), batch_time=batch_time,
+                      data_time=data_time, loss=losses, top1=top1, top5=top5))
 
         epoch_time = batch_time.avg * len(trainloader)  # Time for total training dataset
     else:
