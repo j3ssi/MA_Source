03f47f1e7ef07ab57ab71b00198d9cbdde3a3fc5
j3ssi <jessica.buehler@udo.edu>
2020-02-26 21:58:15 +0100

    time vs. batch size

--- run-script.py
+++ run-script.py
@@ -8,7 +8,7 @@ def main():
         cmdLine = 'python3 main.py '
         cmdLine = cmdLine + '--workers 4 --epochs 1 '
         cmdLine = cmdLine + '--learning-rate 0.1 --schedule 91 136 '
-        cmdLine = cmdLine + '--gpu_id 2 --train_batch ' + str(batch_size*100)
+        cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size*100)
         cmdLine = cmdLine + ' --en_group_lasso --test_batch 100 --sparse_interval 5'
         cmdLine = cmdLine + ' -s ' + str(s) + ' -n ' + str(n) +' -l 3'
         print (cmdLine)
@@ -21,7 +21,7 @@ def main():
         cmdLine = 'python3 main.py '
         cmdLine = cmdLine + '--workers 4 --epochs 1 '
         cmdLine = cmdLine + '--learning-rate 0.1 --schedule 91 136 '
-        cmdLine = cmdLine + '--gpu_id 2 --train_batch ' + str(batch_size*10)
+        cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size*10)
         cmdLine = cmdLine + ' --en_group_lasso --test_batch 100 --sparse_interval 5'
         cmdLine = cmdLine + ' -s ' + str(s) + ' -n ' + str(n) +' -l 3'
         print (cmdLine)
