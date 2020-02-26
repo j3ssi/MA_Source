32f8429ece41bf2552b1ec12d50eccd40f622cc3
j3ssi <jessica.buehler@udo.edu>
2020-02-26 22:11:46 +0100

    time vs. batch size

--- run-script.py
+++ run-script.py
@@ -4,11 +4,11 @@ def main():
     s = 1
     n = 2
 
-    for batch_size in range(1,88):
+    for batch_size in range(1,881):
         cmdLine = 'python3 main.py '
         cmdLine = cmdLine + '--workers 4 --epochs 1 '
         cmdLine = cmdLine + '--learning-rate 0.1 --schedule 91 136 '
-        cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size*100)
+        cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size*10)
         cmdLine = cmdLine + ' --en_group_lasso --test_batch 100 --sparse_interval 5'
         cmdLine = cmdLine + ' -s ' + str(s) + ' -n ' + str(n) +' -l 3'
         print (cmdLine)
