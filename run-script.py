eac0d4e46890e3a435f421b028696693541b6b2b
j3ssi <jessica.buehler@udo.edu>
2020-03-12 13:54:22 +0100

    gpu auswahl

--- run-script.py
+++ run-script.py
@@ -44,7 +44,7 @@ def main():
     for batch_size in range(1, 70):
         cmdLine = 'python3 main.py '
         cmdLine = cmdLine + '--workers 4 --epochs 1 --fp16'
-        cmdLine = cmdLine + '--learning-rate 0.1 --schedule 91 136 '
+        cmdLine = cmdLine + ' --learning-rate 0.1 --schedule 91 136 '
         cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size * 10)
         cmdLine = cmdLine + ' --test_batch 100 -s ' + str(s) + ' -n ' + str(n) + ' -l 3'
         print(cmdLine)
