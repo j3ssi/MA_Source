bddfa5fe0fbf8552e7410385ec38ef9355c5a6f8
j3ssi <jessica.buehler@udo.edu>
2020-03-16 22:10:02 +0100

    gpu auswahl

--- main.py
+++ main.py
@@ -492,7 +492,7 @@ def main():
                     # if epoch in args.schedule:
                     #     adjust_learning_rate(optimizer, epoch)
 
-                    # print('\nEpoch: [%d | %d] LR: %f' % (epoch, args.epochs, state['lr']))
+            print('\nEpoch: [%d | %d] LR: %f' % (epoch, args.epochs, state['lr']))
             start = time.time()
             train_loss, train_acc, lasso_ratio, train_epoch_time = train(trainloader, model, criterion,
                                                                                  optimizer,
