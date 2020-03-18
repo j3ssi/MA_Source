6265cc20ff42637420ac397727bdfe796197f51c
j3ssi <jessica.buehler@udo.edu>
2020-03-18 17:14:45 +0100

    + --test

--- run-script.py
+++ run-script.py
@@ -15,24 +15,24 @@ def main():
     #     print(cmdLine)
     #     os.system(cmdLine)
 
+    # s = 3
+    # n = 3
+    #
+    # for batch_size in range(1, 20):
+    #     cmdLine = 'python3 main.py '
+    #     cmdLine = cmdLine + '--workers 4 --epochs 50 --O1 --test '
+    #     cmdLine = cmdLine + ' --learning-rate 0.1 '
+    #     cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size * 100)
+    #     cmdLine = cmdLine + ' --test_batch 100 -s ' + str(s) + ' -n ' + str(n) + ' -l 3'
+    #     print(cmdLine)
+    #     os.system(cmdLine)
+
     s = 3
     n = 3
 
-    for batch_size in range(1, 20):
+    for batch_size in range(1, 30):
         cmdLine = 'python3 main.py '
-        cmdLine = cmdLine + '--workers 4 --epochs 50 --O1 --test '
-        cmdLine = cmdLine + ' --learning-rate 0.1 '
-        cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size * 100)
-        cmdLine = cmdLine + ' --test_batch 100 -s ' + str(s) + ' -n ' + str(n) + ' -l 3'
-        print(cmdLine)
-        os.system(cmdLine)
-
-    s = 3
-    n = 3
-
-    for batch_size in range(1, 20):
-        cmdLine = 'python3 main.py '
-        cmdLine = cmdLine + '--workers 4 --epochs 50 --O2'
+        cmdLine = cmdLine + '--workers 4 --epochs 50 --O2 --test'
         cmdLine = cmdLine + ' --learning-rate 0.1 '
         cmdLine = cmdLine + '--batchTrue --batch_size ' + str(batch_size * 100)
         cmdLine = cmdLine + ' --test_batch 100 -s ' + str(s) + ' -n ' + str(n) + ' -l 3'
