b9b64ec3b7292340217b041021164f50acab62ba
j3ssi <jessica.buehler@udo.edu>
2020-03-29 16:55:12 +0200

    resnet

--- src/n2n.py
+++ src/n2n.py
@@ -75,14 +75,14 @@ class N2N(nn.Module):
                                                      bias=False,
                                                      stride=1)
                                     self.module_list.append(conv)
-                                    bn = nn.BatchNorm2d(4*sizeOfLayer)
+                                    bn = nn.BatchNorm2d(sizeOfLayer)
                                     self.module_list.append(bn)
                                 else:
                                     conv = nn.Conv2d(sizeOfLayer, sizeOfLayer*4, kernel_size=3, padding=1,
                                                      bias=False,
                                                      stride=1)
                                     self.module_list.append(conv)
-                                    bn = nn.BatchNorm2d(sizeOfLayer)
+                                    bn = nn.BatchNorm2d(4*sizeOfLayer)
                                     self.module_list.append(bn)
 
 
