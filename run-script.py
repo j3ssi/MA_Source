bafa874217bdadadfc5a910a5b1653a39ea90779
j3ssi <jessica.buehler@udo.edu>
2020-04-02 00:53:21 +0200

    run script

--- run-script.py
+++ run-script.py
@@ -10,7 +10,7 @@ def main():
         cmdLine = 'python3 main.py '
         cmdLine = cmdLine + '--workers 4 --epochs 180 --test --cifar10'
         cmdLine = cmdLine + ' --learning-rate 0.1 '
-        cmdLine = cmdLine + ' --batchTrue --batch_size 290 -b '
+        cmdLine = cmdLine + ' --batchTrue --batch_size 209 -b '
         cmdLine = cmdLine + ' --test_batch 100 -s ' + str(s) + ' -n 6,6,6 -l 3'
         print(cmdLine)
         os.system(cmdLine)
