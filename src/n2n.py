883cd0e3d5d61f1bae8570f689fad7b74df13d34
j3ssi <jessica.buehler@udo.edu>
2020-04-18 06:18:53 +0200

    delete module from modulelist

--- src/n2n.py
+++ src/n2n.py
@@ -616,7 +616,7 @@ class N2N(nn.Module):
                 stagesO.append([])
                 stagesO[-1].append(layer)
 
-        print(f'tempStagesI: {StagesI}')
+        print(f'stagesI: {stagesI}')
 
         print(f'stagesO: {stagesO}')
         return stagesI, stagesO
