0199e98319619c0e904b13877f92d9abad0b5de3
j3ssi <jessica.buehler@udo.edu>
2020-04-20 16:27:12 +0200

    delete module from modulelist

--- run-script.py
+++ run-script.py
@@ -14,6 +14,7 @@ def main():
 
 # python3 main.py -j 4 --epochs 180  -s 3 -l 2 -n 5,5,5 --cifar10 --test --sparse_interval 5 --threshold 0.01 --en_group_lasso --batchTrue --batch_size 3900
 
+# python3 main.py -j 4 --epochs 180  -s 3 -l 2 -n 5,5,5 --cifar10 --test --batchTrue --batch_size 3900
 
 
 if __name__ == '__main__':
