7ad9f0a9c4d074848821bec5d660ffbe9bf9ea7e
j3ssi <jessica.buehler@udo.edu>
2020-04-24 21:39:01 +0200

    checkpoint

--- run-script.py
+++ run-script.py
@@ -8,8 +8,9 @@ def main():
             cmdLine = 'python3 main.py '
             cmdLine = cmdLine + '--workers 0 --checkpoint ./output/prune1 --test --epochs 5 --cifar10 --gpu_id 2 '
             cmdLine = cmdLine + ' --threshold 0.01  --en_group_lasso --batchTrue --batch_size 128 --sparse_interval 5 '
-            cmdLine = cmdLine + ' --var_group_lasso_coeff 0.2 --test_batch 100 -s ' + str(s) + ' -n 5,5,5 -l 2 --epochs'
+            cmdLine = cmdLine + ' --var_group_lasso_coeff 0.2 --test_batch 100 -s ' + str(s) + ' -n 5,5,5 -l 2 '
             cmdLine = cmdLine + ' --gpu_id 2 --cifar10 --learning-rate 0.1 --sparse_interval 5  --learning-rate 0.1 '
+            cmdLine = cmdLine + ' --epochsFromBegin ' + str(j*5)
             if(j>0):
                 cmdLine = cmdLine + ' --resume ./output/prune1/checkpoint.pth.tar'
             print(cmdLine)
