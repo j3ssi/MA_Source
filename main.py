9b818ca6c5306867fce9e4dba8c4a3e955064a75
j3ssi <jessica.buehler@udo.edu>
2020-04-25 03:20:41 +0200

    checkpoint

--- main.py
+++ main.py
@@ -292,7 +292,6 @@ def main():
 
     # dynamic resnet modell
 
-    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
     title = 'prune' + str(args.epochsFromBegin)
     if args.resume:
