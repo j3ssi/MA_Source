35c5714ef207bde239229fa0ee9e347770cf8fe6
j3ssi <jessica.buehler@udo.edu>
2020-04-27 15:56:59 +0200

    parameter

--- main.py
+++ main.py
@@ -534,67 +534,67 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
             # print(f'After loss')
 
             # lasso penalty
-            init_batch = batch_idx == 0 and epoch == 1
+        init_batch = batch_idx == 0 and epoch == 1
 
-            if args.en_group_lasso:
-                if args.global_group_lasso:
-                    lasso_penalty = get_group_lasso_global(model)
-                else:
-                    lasso_penalty = get_group_lasso_group(model)
-
-                # Auto-tune the group-lasso coefficient @first training iteration
-                coeff_dir = os.path.join(args.coeff_container, 'cifar')
-                if init_batch:
-                    args.grp_lasso_coeff = args.var_group_lasso_coeff * loss.item() / (
-                            lasso_penalty * (1 - args.var_group_lasso_coeff))
-                    grp_lasso_coeff = torch.autograd.Variable(args.grp_lasso_coeff)
-
-                    if not os.path.exists(coeff_dir):
-                        os.makedirs(coeff_dir)
-                    with open(os.path.join(coeff_dir, str(args.var_group_lasso_coeff)), 'w') as f_coeff:
-                        f_coeff.write(str(grp_lasso_coeff.item()))
-
-                else:
-                    with open(os.path.join(coeff_dir, str(args.var_group_lasso_coeff)), 'r') as f_coeff:
-                        for line in f_coeff:
-                            grp_lasso_coeff = float(line)
-
-                lasso_penalty = lasso_penalty * grp_lasso_coeff
+        if args.en_group_lasso:
+            if args.global_group_lasso:
+                lasso_penalty = get_group_lasso_global(model)
             else:
-                lasso_penalty = 0.
+                lasso_penalty = get_group_lasso_group(model)
+            print(f'Lasso Penalty: {lasso_penalty}')
+            # Auto-tune the group-lasso coefficient @first training iteration
+            coeff_dir = os.path.join(args.coeff_container, 'cifar')
+            if init_batch:
+                args.grp_lasso_coeff = args.var_group_lasso_coeff * loss.item() / (
+                            lasso_penalty * (1 - args.var_group_lasso_coeff))
+                grp_lasso_coeff = torch.autograd.Variable(args.grp_lasso_coeff)
+
+                if not os.path.exists(coeff_dir):
+                    os.makedirs(coeff_dir)
+                with open(os.path.join(coeff_dir, str(args.var_group_lasso_coeff)), 'w') as f_coeff:
+                    f_coeff.write(str(grp_lasso_coeff.item()))
+
+            else:
+                with open(os.path.join(coeff_dir, str(args.var_group_lasso_coeff)), 'r') as f_coeff:
+                    for line in f_coeff:
+                        grp_lasso_coeff = float(line)
+            print(f'Grp lasso coeff: {grp_lasso_coeff}')
+            lasso_penalty = lasso_penalty * grp_lasso_coeff
+        else:
+            lasso_penalty = 0.
             # print(f'nach group lasso')
             # Group lasso calcution is not performance-optimized => Ignore from execution time
-            loss += lasso_penalty
+        loss += lasso_penalty
             # measure accuracy and record loss
-            prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
-            losses.update(loss.item(), inputs.size(0))
-            top1.update(prec1.item(), inputs.size(0))
-            top5.update(prec5.item(), inputs.size(0))
-            lasso_ratio.update(lasso_penalty / loss.item(), inputs.size(0))
+        prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
+        losses.update(loss.item(), inputs.size(0))
+        top1.update(prec1.item(), inputs.size(0))
+        top5.update(prec5.item(), inputs.size(0))
+        lasso_ratio.update(lasso_penalty / loss.item(), inputs.size(0))
 
-            # compute gradient and do SGD step
-            if args.O1 or args.O2 or args.O3:
-                with amp.scale_loss(loss, optimizer) as scaled_loss:
-                    scaled_loss.backward()
-            else:
-                loss.backward()
-                # print(f'After backward')
-            optimizer.step()
+        # compute gradient and do SGD step
+        if args.O1 or args.O2 or args.O3:
+            with amp.scale_loss(loss, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            loss.backward()
+            # print(f'After backward')
+        optimizer.step()
 
-            # print(f'After Step')
-            # measure elapsed time
-            batch_time.update(time.time() - end - data_load_time)
-            end = time.time()
+        # print(f'After Step')
+        # measure elapsed time
+        batch_time.update(time.time() - end - data_load_time)
+        end = time.time()
 
-            if batch_idx % args.print_freq == 0:
-                print('Epoch: [{0}][{1}/{2}]\t'
-                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
-                      'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
-                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
-                      'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
-                      'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
-                    epoch, batch_idx, len(trainloader), batch_time=batch_time,
-                    data_time=data_time, loss=losses, top1=top1, top5=top5))
+        if batch_idx % args.print_freq == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@5 {top5.val:.3f} ({top5.avg:.3f})'.format(
+                  epoch, batch_idx, len(trainloader), batch_time=batch_time,
+                  data_time=data_time, loss=losses, top1=top1, top5=top5))
     # print(f'For fertig!!')
     epoch_time = batch_time.avg * len(trainloader)  # Time for total training dataset
     return losses.avg, top1.avg, lasso_ratio.avg, epoch_time
