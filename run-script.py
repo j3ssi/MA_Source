743504e2fe9c47518222cbc06e5ffa27e8dfdfbe
j3ssi <jessica.buehler@udo.edu>
2020-04-27 22:13:53 +0200

    parameter

--- run-script.py
+++ run-script.py
@@ -1,12 +1,20 @@
+import argparse
 import os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--checkPointNumber', default=1, type=int, help='number of GPUs used in training')
+args = parser.parse_args()
+
+
 
 def main():
 
+    checkpoint = './output/prune' + str(args.checkPointNumber)
     s = 3
-    for i in range(0,1):
+    for i in range(0,5):
         for j in range(0, 36):
             cmdLine = 'python3 main.py '
-            cmdLine = cmdLine + '--workers 0 --checkpoint ./output/prune1 --test --epochs 5 --cifar10 --gpu_id 2 '
+            cmdLine = cmdLine + '--workers 0 --checkpoint ' + checkpoint + ' --test --epochs 5 --cifar10 --gpu_id 2 '
             cmdLine = cmdLine + ' --threshold 0.01  --en_group_lasso --batchTrue --batch_size 128 --sparse_interval 5 '
             cmdLine = cmdLine + ' --var_group_lasso_coeff 0.2 --test_batch 100 -s ' + str(s) + ' -n 5,5,5 -l 2 '
             cmdLine = cmdLine + ' --gpu_id 2 --cifar10 --learning-rate 0.1 --sparse_interval 5  --learning-rate 0.1 '
