f7d1e930f608fc7d80440d88944a668cb0bb6b63
j3ssi <jessica.buehler@udo.edu>
2020-05-13 01:09:19 +0200

    batch size

--- main.py
+++ main.py
@@ -456,7 +456,8 @@ def main():
                 'epoch': args.epochs + start_epoch,
                 'acc': test_acc,
                 'best_acc': best_acc,
-                'optimizer': optimizer.state_dict(),
+                'optimizer': optimizer.state_dict(),},
+                is_best,
                 checkpoint=args.checkpoint)
             # Leave unique checkpoint of pruned models druing training
             if epoch % args.save_checkpoint == 0:
