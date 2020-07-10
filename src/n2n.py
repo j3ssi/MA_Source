4d94dde832fdd2ff7c58b4f5d2a1c228f2e80930
j3ssi <jessica.buehler@udo.edu>
2020-07-10 19:14:56 +0200

    width of first layer dyn

--- src/n2n.py
+++ src/n2n.py
@@ -12,7 +12,7 @@ class N2N(nn.Module):
     def __init__(self, num_classes, numOfStages, numOfBlocksinStage, layersInBlock,
                  first, bottleneck, widthofFirstLayer =16, model=None, archNums=None):
         super(N2N, self).__init__()
-        self.device = torch.device("cuda:0")
+        # self.device = torch.device("cuda:0")
         self.numOfStages = numOfStages
         self.oddLayers = []
         self.numOfBlocksinStage = numOfBlocksinStage
