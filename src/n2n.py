5156b7214fec878aaebd00d68783fa06012caad0
j3ssi <jessica.buehler@udo.edu>
2020-07-20 21:43:05 +0200

    wider

--- src/n2n.py
+++ src/n2n.py
@@ -782,6 +782,7 @@ class N2N(nn.Module):
 
                 if not random_init:
                     for idx, d in tracking.items():
+                        print(f'type: of d: {d.type()}')
                         for item in d:
                             print(f'item:{item}')
                             # itemIndex = listindices.index(item)
