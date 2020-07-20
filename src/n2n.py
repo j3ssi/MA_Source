2e94d1193013926acdb91763f3f25f09ebbf7b9b
j3ssi <jessica.buehler@udo.edu>
2020-07-20 23:52:43 +0200

    wider

--- src/n2n.py
+++ src/n2n.py
@@ -804,7 +804,7 @@ class N2N(nn.Module):
                 w22 = torch.FloatTensor(dw2).cuda()
                 nw1 = torch.cat((w1, w11),dim=0)
                 nw2 = torch.cat((w2, w22),dim=0)
-                print(f'dim w1: {nw1.shape()}; dim w2: {nw2.shape()}')
+                print(f'dim w1: {nw1.size()}; dim w2: {nw2.size()}')
                 rm = torch.FloatTensor(dbn1rm).cuda()
                 rm1 = torch.FloatTensor(listOfRunningMean).cuda()
                 nbn1rm = torch.cat((rm1, rm), dim=0)
