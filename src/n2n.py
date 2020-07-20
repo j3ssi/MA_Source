c24d01ca61b5a09ee59d24faa8386830c65b8723
j3ssi <jessica.buehler@udo.edu>
2020-07-20 19:04:12 +0200

    wider

--- src/n2n.py
+++ src/n2n.py
@@ -790,6 +790,7 @@ class N2N(nn.Module):
                         for item in d:
                             print(f'item:{item}')
                             itemIndex = listindices.index(int(item))
+                            print(f'item index: {itemIndex}')
                             listindices.remove(itemIndex)
                             c= dw2[itemIndex]
                             # print(f'c:{c}')
