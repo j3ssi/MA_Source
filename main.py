52ab003a6a837982fa998861a29a458e755dd3fd
j3ssi <jessica.buehler@udo.edu>
2020-07-23 02:19:47 +0200

    wider

--- main.py
+++ main.py
@@ -332,7 +332,7 @@ def main():
         assert args.numOfStages == len(
             listofBlocks), 'Liste der Blöcke pro Stage sollte genauso lang sein wie Stages vorkommen!!!'
 
-        model = n2n.N2N(num_classes, args.numOfStages, listofBlocks, args.layersInBlock, True, args.bottleneck,  widthofFirstLayer =16, model=None, archNums=None, listOfWidths)
+        model = n2n.N2N(num_classes, args.numOfStages, listofBlocks, args.layersInBlock, True, args.bottleneck,  widthofFirstLayer =16, model=None, archNums=None, widthOfLayers = listOfWidths)
 
         print(f'device count: {torch.cuda.device_count()}')
         model.cuda()
