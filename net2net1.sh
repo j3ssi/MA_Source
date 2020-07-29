7d193622e1ac8e13437731c43edb88bb0632a5f6
j3ssi <jessica.buehler@udo.edu>
2020-07-29 23:15:59 +0200

    wider

--- net2net1.sh
+++ net2net1.sh
@@ -2,11 +2,11 @@
 
 
 echo "j: 1 bis 5"
-python3 main.py -j 6 --checkpoint ./output/experimente2/net2netwider1_$1 --epochs 1 --batchTrue --batch_size 256 --n2n --wider   -s 3 -l 2 -n 5,5,5 --sparse_interval 0 --widthOfAllLayers 8,16,32  --cifar10 --test --saveModell --test_batch 200 --epochsFromBegin 0 --pathToModell ./output/experimente2/net2netwider1_$1/model.nn
+python3 main.py -j 6 --checkpoint ./output/experimente2/net2netwider1_$1 --epochs 5 --batchTrue --batch_size 256 -s 3 -l 2 -n 5,5,5 --sparse_interval 0 --widthOfAllLayers 8,16,32  --cifar10 --test --saveModell --test_batch 200 --epochsFromBegin 0 --pathToModell ./output/experimente2/net2netwider1_$1/model.nn
 sleep 20
 
 echo "j: 6 bis 10"
-python3 main.py -j 6 --checkpoint ./output/experimente2/net2netwider1_$1 --epochs 5 --batchTrue --batch_size 256  -s 3 -l 2 -n 5,5,5 --sparse_interval 0 --widthOfAllLayers 8,16,32  --cifar10 --test --saveModell --test_batch 200 --epochsFromBegin 5 --pathToModell ./output/experimente2/net2netwider1_$1/model.nn  --resume ./output/experimente2/net2netwider1_$1/checkpoint.pth.tar
+python3 main.py -j 6 --checkpoint ./output/experimente2/net2netwider1_$1 --epochs 5 --batchTrue --batch_size 256 -s 3 -l 2 -n 5,5,5 --sparse_interval 0 --widthOfAllLayers 8,16,32  --cifar10 --test --saveModell --test_batch 200 --epochsFromBegin 5 --pathToModell ./output/experimente2/net2netwider1_$1/model.nn  --resume ./output/experimente2/net2netwider1_$1/checkpoint.pth.tar
 sleep 20
 
 echo "j: 11 bis 15"
