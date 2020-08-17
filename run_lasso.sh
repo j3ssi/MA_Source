09112c4447ae18daf8d39abc974737c67c5b9881
j3ssi <jessica.buehler@udo.edu>
2020-08-17 23:28:39 +0200

    print

--- run_lasso.sh
+++ run_lasso.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-echo "Lasso 0.05"
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 ./run_lasso_005.sh $1
-sleep 20
-
-echo "Lasso 0.1"
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 ./run_lasso_01.sh $1
-sleep 20
 
 echo "Lasso 0.15"
 CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=2 ./run_lasso_015.sh $1
