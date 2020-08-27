49456d2b7a0daca297b05f6872532fdd399762bd
j3ssi <jessica.buehler@udo.edu>
2020-08-27 20:08:18 +0200

    scripte

--- run_reconf.sh
+++ run_reconf.sh
@@ -1,14 +1,72 @@
 #!/bin/sh
 
-echo "reconf 2"
-./run_reconf_2.sh $1
+echo "reconf 2 1"
+./run_reconf_2.sh 1
 sleep 20
 
 
-echo "reconf 5"
-./run_reconf_5.sh $1
+echo "reconf 5 1"
+./run_reconf_5.sh 1
 sleep 20
 
 
-echo "reconf 10"
-./run_reconf_10.sh $1
+echo "reconf 10 1"
+./run_reconf_10.sh 1
+
+
+echo "reconf 2 2"
+./run_reconf_2.sh 2
+sleep 20
+
+
+echo "reconf 5 2"
+./run_reconf_5.sh 2
+sleep 20
+
+
+echo "reconf 10 2"
+./run_reconf_10.sh 2
+
+
+echo "reconf 2 3"
+./run_reconf_2.sh 3
+sleep 20
+
+
+echo "reconf 5 3"
+./run_reconf_5.sh 3
+sleep 20
+
+
+echo "reconf 10 3"
+./run_reconf_10.sh 3
+
+
+
+echo "reconf 2 4"
+./run_reconf_2.sh 4
+sleep 20
+
+
+echo "reconf 5 4"
+./run_reconf_5.sh 4
+sleep 20
+
+
+echo "reconf 10 4"
+./run_reconf_10.sh 4
+
+
+echo "reconf 2 5"
+./run_reconf_2.sh 5
+sleep 20
+
+
+echo "reconf 5 5"
+./run_reconf_5.sh 5
+sleep 20
+
+
+echo "reconf 10 5"
+./run_reconf_10.sh 5
+
