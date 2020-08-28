652323606f9eeaa12d03cb31517cd29fecfcc814
j3ssi <jessica.buehler@udo.edu>
2020-08-28 21:38:00 +0200

    scripts

--- run_lr.sh
+++ run_lr.sh
@@ -1,22 +1,104 @@
 #!/bin/sh
 
-echo "lr 0.2"
-./run_lr2.sh $1
-sleep 20
+echo "lr 0.2 1"
+./run_lr2.sh 1
+sleep 3
+
+echo "lr 0.2 2"
+./run_lr2.sh 2
+sleep 3
+
+echo "lr 0.2 3"
+./run_lr2.sh 3
+sleep 3
+
+echo "lr 0.2 4"
+./run_lr2.sh 4
+sleep 3
+
+echo "lr 0.2 5"
+./run_lr2.sh 5
+sleep 3
 
 
-echo "lr 0.1"
-./run_lr1.sh $1
-sleep 20
+echo "lr 0.1 1"
+./run_lr1.sh 1
+sleep 3
+
+echo "lr 0.1 2"
+./run_lr1.sh 2
+sleep 3
+
+echo "lr 0.1 3"
+./run_lr1.sh 3
+sleep 3
+
+echo "lr 0.1 4"
+./run_lr1.sh 4
+sleep 3
+
+echo "lr 0.1 5"
+./run_lr1.sh 5
+sleep 3
 
 
-echo "lr 0.05"
-./run_lr05.sh $1
-sleep 20
+echo "lr 0.05 1"
+./run_lr05.sh 1
+sleep 3
 
-echo "lr 0.025"
-./run_lr025.sh $1
-sleep 20
+echo "lr 0.05 2"
+./run_lr05.sh 2
+sleep 3
 
-echo "Thres 0.0125"
-./run_lr0125.sh $1
+echo "lr 0.05 3"
+./run_lr05.sh 3
+sleep 3
+
+echo "lr 0.05 4"
+./run_lr05.sh 4
+sleep 3
+
+echo "lr 0.05 5"
+./run_lr05.sh 5
+sleep 3
+
+echo "lr 0.025 1"
+./run_lr025.sh 1
+sleep 3
+
+echo "lr 0.025 2"
+./run_lr025.sh 2
+sleep 3
+
+echo "lr 0.025 3"
+./run_lr025.sh 3
+sleep 3
+
+echo "lr 0.025 4"
+./run_lr025.sh 4
+sleep 3
+
+echo "lr 0.025 5"
+./run_lr025.sh 5
+sleep 3
+
+
+echo "Thres 0.0125 1"
+./run_lr0125.sh 1
+sleep 3
+
+echo "Thres 0.0125 2"
+./run_lr0125.sh 2
+sleep 3
+
+echo "Thres 0.0125 3"
+./run_lr0125.sh 3
+sleep 3
+
+echo "Thres 0.0125 4"
+./run_lr0125.sh 4
+sleep 3
+
+echo "Thres 0.0125 5"
+./run_lr0125.sh 5
+sleep 3
