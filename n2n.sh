0d025e9c074475bfec46f5d00c5de689490a9c8d
j3ssi <jessica.buehler@udo.edu>
2020-09-02 06:20:28 +0200

    6.1

---
+++ n2n.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+echo "Net2Net 1"
+./net2net1.sh 1
+sleep 20
+
+echo "Net2Net 2"
+./net2net1.sh 2
+sleep 20
+
+echo "Net2Net 3"
+./net2net1.sh 3
+sleep 20
+
+echo "Net2Net 4"
+./net2net1.sh 4
+sleep 20
+
+echo "Net2Net 5"
+./net2net1.sh 5
+sleep 20
