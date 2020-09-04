121f91320853f9138253ec23fcff9689c73923c8
j3ssi <jessica.buehler@udo.edu>
2020-09-04 02:33:53 +0200

    net2net

---
+++ run_baselineLr.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+echo "run_baselineLr.sh 1"
+time ./run_baselineLr.sh.sh 1
+sleep 20
+
+echo "run_baselineLr.sh 2"
+time ./run_baselineLr.sh.sh 2
+sleep 20
+
+echo "run_baselineLr.sh 3"
+time ./run_baselineLr.sh.sh 3
+sleep 20
+
+echo "run_baselineLr.sh 4"
+time ./run_baselineLr.sh.sh 4
+sleep 20
+
+echo "run_baselineLr.sh 5"
+time ./run_baselineLr.sh.sh 5
+sleep 20
