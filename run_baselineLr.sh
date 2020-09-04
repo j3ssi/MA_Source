c140a6090daab314b8822c46083258382d0f80c4
j3ssi <jessica.buehler@udo.edu>
2020-09-04 02:55:14 +0200

    baselineLr

--- run_baselineLr.sh
+++ run_baselineLr.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-echo "run_baselineLr.sh 1"
-time ./run_baselineLr.sh.sh 1
+echo "baselineLr.sh 1"
+time ./run_baselineL.sh 1
 sleep 20
 
-echo "run_baselineLr.sh 2"
-time ./run_baselineLr.sh.sh 2
+echo "baselineLr.sh 2"
+time ./baselineLr.sh 2
 sleep 20
 
-echo "run_baselineLr.sh 3"
-time ./run_baselineLr.sh.sh 3
+echo "baselineLr.sh 3"
+time ./baselineLr.sh 3
 sleep 20
 
-echo "run_baselineLr.sh 4"
-time ./run_baselineLr.sh.sh 4
+echo "baselineLr.sh 4"
+time ./baselineLr.sh 4
 sleep 20
 
-echo "run_baselineLr.sh 5"
-time ./run_baselineLr.sh.sh 5
+echo "baselineLr.sh 5"
+time ./baselineLr.sh 5
 sleep 20
