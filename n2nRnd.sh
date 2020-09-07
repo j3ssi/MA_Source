f66d4f75ad6302654de1ba65c0b08155ed9b76d5
j3ssi <jessica.buehler@udo.edu>
2020-09-08 00:11:24 +0200

    net2net rnd

---
+++ n2nRnd.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+echo "Net2Net Random 1"
+./net2net1Rnd.sh 1
+sleep 20
+
+echo "Net2Net Random 2"
+./net2ner1Rnd.sh 2
+sleep 20
+
+echo "Net2Net Random 3"
+./net2net1Rnd.sh 3
+sleep 20
+
+echo "Net2Net Random 4"
+./net2net1Rnd.sh 4
+sleep 20
+
+echo "Net2Net Random 5"
+./net2net1Rnd.sh 5
+sleep 20
