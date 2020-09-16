e0a60b3a4fe695a9d010c80527af9213ff47aea6
j3ssi <jessica.buehler@udo.edu>
2020-09-16 06:53:35 +0200

    lr

--- src/n2n1.py
+++ src/n2n1.py
@@ -373,7 +373,7 @@ class N2N(nn.Module):
     def forward(self, x,epoch ):
         # print(f'ArchNums: {self.archNums}')
         # First layer
-        if(epoch%20)==0:
+        if(epoch%20)==0and epoch>0:
             for i in range(len(self.paramList)):
                 self.paramList[i].requires_grad=True
                 self.paramList1[i].requires_grad=True
