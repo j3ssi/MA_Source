ed0b7919b13faded4de855cb478a1d706cebe290
j3ssi <jessica.buehler@udo.edu>
2020-09-21 09:35:47 +0200

    lr

--- src/n2n1.py
+++ src/n2n1.py
@@ -1047,7 +1047,7 @@ class N2N(nn.Module):
             if i==c:
 
                 param1 = nn.Parameter(torch.ones(1))
-                param1.fill(0.5)
+                param1.data.fill_(0.5)
                 paramListTmp.append(param1, requires_grad=False)
                 paramListTmp1.append(param1, requires_grad=False)
 
