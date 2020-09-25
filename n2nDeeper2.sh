faf530ffe1d23262644ba49c406c4bbc98c0671e
j3ssi <jessica.buehler@udo.edu>
2020-09-26 01:19:20 +0200

    n2n

---
+++ n2nDeeper2.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+
+echo "Net2Net Deeper 21"
+./net2net2Deeper.sh 1
+sleep 20
+
+echo "Net2Net Deeper 22"
+./net2ner2Deeper.sh 2
+sleep 20
+
+echo "Net2Net Deeper 23"
+./net2net2Deeper.sh 3
+sleep 20
+
+echo "Net2Net Deeper 24"
+./net2net2Deeper.sh 4
+sleep 20
+
+echo "Net2Net Deeper 25"
+./net2net2Deeper.sh 5
+sleep 20
