428db09e84b188d0b16d5002b51790a230933ba3
j3ssi <jessica.buehler@udo.edu>
2020-09-29 23:10:54 +0200

    deeper

--- run_reconf.sh
+++ run_reconf.sh
@@ -1,74 +1,74 @@
 #!/bin/sh
 
-echo "reconf 2 1"
-./run_reconf_2.sh 1
+echo "reconf 2 6"
+./run_reconf_2.sh 6
 sleep 20
 
 
-echo "reconf 5 1"
-./run_reconf_5.sh 1
+echo "reconf 5 6"
+./run_reconf_5.sh 6
 sleep 20
 
 
-echo "reconf 10 1"
-./run_reconf_10.sh 1
+echo "reconf 10 6"
+./run_reconf_10.sh 6
 
 
-echo "reconf 2 2"
-./run_reconf_2.sh 2
+echo "reconf 2 7"
+./run_reconf_2.sh 7
 sleep 20
 
 
-echo "reconf 5 2"
-./run_reconf_5.sh 2
+echo "reconf 5 7"
+./run_reconf_5.sh 7
 sleep 20
 
 
-echo "reconf 10 2"
-./run_reconf_10.sh 2
+echo "reconf 10 7"
+./run_reconf_10.sh 7
 
 
-echo "reconf 2 3"
-./run_reconf_2.sh 3
+echo "reconf 2 8"
+./run_reconf_2.sh 8
 sleep 20
 
 
-echo "reconf 5 3"
-./run_reconf_5.sh 3
+echo "reconf 5 8"
+./run_reconf_5.sh 8
 sleep 20
 
 
-echo "reconf 10 3"
-./run_reconf_10.sh 3
+echo "reconf 10 8"
+./run_reconf_10.sh 8
 
 
 
-echo "reconf 2 4"
-./run_reconf_2.sh 4
+echo "reconf 2 9"
+./run_reconf_2.sh 9
 sleep 20
 
 
-echo "reconf 5 4"
-./run_reconf_5.sh 4
+echo "reconf 5 9"
+./run_reconf_5.sh 9
 sleep 20
 
 
-echo "reconf 10 4"
-./run_reconf_10.sh 4
+echo "reconf 10 9"
+./run_reconf_10.sh 9
 
 
-echo "reconf 2 5"
-./run_reconf_2.sh 5
+echo "reconf 2 10"
+./run_reconf_2.sh 10
 sleep 20
 
 
-echo "reconf 5 5"
-./run_reconf_5.sh 5
+echo "reconf 5 10"
+./run_reconf_5.sh 10
 sleep 20
 
 
-echo "reconf 10 5"
-./run_reconf_10.sh 5
+echo "reconf 10 10"
+./run_reconf_10.sh 10
 
 
 
