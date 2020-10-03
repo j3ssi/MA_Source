351f1f596725529fd9241e2576418bbf67c8989c
j3ssi <jessica.buehler@udo.edu>
2020-10-03 16:17:05 +0200

    lr

--- src/n2n.py
+++ src/n2n.py
@@ -1072,7 +1072,7 @@ class N2N(nn.Module):
                 if (block +1) == len(archNum):
                     j += 2
                     print(f'block == len(archNum)')
-                if block==0 and stage>0 and pos+1 <= archNum[blocks]:
+                if block==0 and stage>0 and pos+1 <= archNum[block]:
                     print(f'drin!!')
                     j += 2 * archNum[block] - 2 * pos
 
