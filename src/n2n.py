f469a03853c6ae2c8a79f3621b803c229b93f487
j3ssi <jessica.buehler@udo.edu>
2020-10-09 21:50:32 +0200

    sequential

--- src/n2n.py
+++ src/n2n.py
@@ -819,6 +819,7 @@ class N2N(nn.Module):
                 newModule_list.append( nn.Sequential(*seq))
                 # print(f'danach: {newModule_list[i]}')
                 print(f'i: {i}')
+                l = i
                 if ( i - 2 ) % 5 == 0 and ( i - 2 ) // 5 > 0:
                     newModule_list.append(self.module_list[i +1])
                     m =( ( i - 2 ) // 5 ) - 1
