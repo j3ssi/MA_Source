5b540cf7b1b8613b7ffc8cbcdb111d40db2569ad
j3ssi <jessica.buehler@udo.edu>
2020-10-10 08:32:55 +0200

    sequential

--- src/n2n.py
+++ src/n2n.py
@@ -770,12 +770,12 @@ class N2N(nn.Module):
 
         print(f'tmp: {stages}')
         stage = 0
-        for i in range(len(self.module_list)):
+        for i in range( len( self.module_list ) ):
             if i>3 and blockComp:
                 print(f'skip: {i}')
                 blockComp = False
+                newModule_list.append( self.module_list[ i ] )
                 continue
-
             print(f'stages: {stages[k]}')
 
 
