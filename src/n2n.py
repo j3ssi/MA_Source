328129639f5586b4fcc249e357618eaf310961fa
j3ssi <jessica.buehler@udo.edu>
2020-10-10 05:47:46 +0200

    sequential

--- src/n2n.py
+++ src/n2n.py
@@ -765,6 +765,7 @@ class N2N(nn.Module):
         for s in range(0, self.numOfStages):
             for t in range(len(self.archNums[s])):
                 tmp += self.archNums[s][t]
+            tmp += 1
             stages.append(tmp)
 
         print(f'tmp: {stages}')
