d3842dcd895b9b99718cdddb6e86f8ec7e7f3a18
j3ssi <jessica.buehler@udo.edu>
2020-10-15 13:34:01 +0200

    deeper

--- src/n2n.py
+++ src/n2n.py
@@ -1429,7 +1429,7 @@ class N2N(nn.Module):
         # self.module_list = newModule_list
         # print(f'Self: {self}')
         batch_size = 1
-        input = torch.randn(1,32, 32,3)
+        input = torch.randn(1, 3, 32, 32)
         self.forward(input, True)
         return self
 
