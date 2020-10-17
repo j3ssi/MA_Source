df004e2f5fca2bdac0e675e232b1c221700e5c5c
j3ssi <jessica.buehler@udo.edu>
2020-10-17 05:53:09 +0200

    wider

--- n2nDeeper.sh
+++ n2nDeeper.sh
@@ -4,19 +4,3 @@
 echo "Net2Net Deeper 1"
 ./net2net1Deeper.sh 1
 sleep 20
-
-echo "Net2Net Deeper 2"
-./net2ner1Deeper.sh 2
-sleep 20
-
-echo "Net2Net Deeper 3"
-./net2net1Deeper.sh 3
-sleep 20
-
-echo "Net2Net Deeper 4"
-./net2net1Deeper.sh 4
-sleep 20
-
-echo "Net2Net Deeper 5"
-./net2net1Deeper.sh 5
-sleep 20
