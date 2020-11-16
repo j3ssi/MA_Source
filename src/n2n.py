8bdea21d91b5943efc786029a9a7d86268dbb7b6
j3ssi <jessica.buehler@udo.edu>
2020-11-16 23:57:50 +0100

    logs

--- src/n2n.py
+++ src/n2n.py
@@ -872,7 +872,7 @@ class N2N(nn.Module):
                 i3 = module.kernel_size[1]
                 old_w1 = module.weight.data.clone().cpu().detach().numpy()
                 n = i2 * i3 * module.out_channels
-                new_w1 = n * np.random.randn(shape=(i0,i1,i2,i3), dtype = old_w1.dtype)
+                new_w1 = n * np.random.randn(i0,i1,i2,i3)
                 for k in range(0, module.out_channels):
                     new_w1_re = old_w1[k,:,:,:]
                     new_w1 = np.concatenate((new_w1, new_w1_re), axis = 0)
@@ -880,13 +880,13 @@ class N2N(nn.Module):
                 module.data = new_w1
                 new_w2 = module1.weight.data.clone().cpu().detach().numpy()
                 if isinstance(module1, nn.Conv2d):
-                    i0 = int(module1.in_channels * delta_width)
-                    i1 = module1.in_channels
+                    i0 = module1.out_channels
+                    i1 = int( module1.in_channels * delta_width)
                     i2 = module1.kernel_size[0]
                     i3 = module1.kernel_size[1]
                     old_w2 = module1.weight.data.clone().cpu().detach().numpy()
                     n = i2 * i3 * module1.in_channels
-                    new_w2 = n * np.random.randn(shape=(i0, i1, i2, i3), dtype=old_w2.dtype)
+                    new_w2 = n * np.random.randn(i0, i1, i2, i3)
                     for k in range(0, module1.in_channels):
                         new_w2_re = old_w2[:, k, :, :]
                         new_w2 = np.concatenate((new_w2, new_w2_re), axis = 1)
@@ -894,11 +894,11 @@ class N2N(nn.Module):
                     module1.in_channels = int(module1.in_channels * delta_width)
                     module1.data = new_w2
                 elif isinstance(module1, nn.Linear):
-                    i0 = int(module1.out_features * delta_width)
-                    i1 = module1.in_features
+                    i0 = module1.out_features
+                    i1 = int(module1.in_features * delta_width)
                     old_w2 = module1.weight.data.clone().cpu().detach().numpy()
                     n = i1
-                    new_w2 = n * np.random.randn(shape=(i0, i1), dtype=old_w2.dtype)
+                    new_w2 = n * np.random.randn(i0, i1)
                     for k in range(0, module1.in_features):
                         new_w2_re = old_w2[:, k]
                         new_w2 = np.concatenate((new_w2, new_w2_re), axis = 1)
