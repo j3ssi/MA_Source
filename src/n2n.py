def450ddd837763f802902c4d449dd8c60220565
j3ssi <jessica.buehler@udo.edu>
2020-11-16 23:17:53 +0100

    logs

--- src/n2n.py
+++ src/n2n.py
@@ -872,7 +872,7 @@ class N2N(nn.Module):
                 i3 = module.kernel_size[1]
                 old_w1 = module.weight.data.clone().cpu().detach().numpy()
                 n = i2 * i3 * module.out_channels
-                new_w1 = torch.empty(size = (i0,i1,i2,i3), dtype = old_w1.dtype() ).normal_(mean = 0, std = n)
+                new_w1 = torch.empty(size = (i0,i1,i2,i3), dtype = old_w1.dtype ).normal_(mean = 0, std = n)
                 for k in range(0, module.out_channels):
                     new_w1[k,:,:,:] = old_w1[k,:,:,:]
                 module.out_channels = int( module.out_channels * delta_width)
@@ -885,7 +885,7 @@ class N2N(nn.Module):
                     i3 = module1.kernel_size[1]
                     old_w2 = module1.weight.data.clone().cpu().detach().numpy()
                     n = i2 * i3 * module1.in_channels
-                    new_w2 = torch.empty(size=(i0, i1, i2, i3), dtype=old_w2.dtype()).normal_(mean=0, std=n)
+                    new_w2 = torch.empty(size=(i0, i1, i2, i3), dtype=old_w2.dtype).normal_(mean=0, std=n)
                     for k in range(0, module1.in_channels):
                         new_w1[:, k, :, :] = old_w1[:, k, :, :]
                     module1.in_channels = int(module1.in_channels * delta_width)
@@ -895,7 +895,7 @@ class N2N(nn.Module):
                     i1 = module1.in_features
                     old_w2 = module1.weight.data.clone().cpu().detach().numpy()
                     n = i1
-                    new_w2 = torch.empty(size=(i0, i1), dtype=old_w2.dtype()).normal_(mean=0, std=n)
+                    new_w2 = torch.empty(size=(i0, i1), dtype=old_w2.dtype).normal_(mean=0, std=n)
                     for k in range(0, module1.in_features):
                         new_w1[:, k, :, :] = old_w1[:, k, :, :]
                     module1.in_features = int(module1.in_features * delta_width)
