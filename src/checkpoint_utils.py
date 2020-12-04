99ff97a2cd221208b7386c9e7f20d29914731967
j3ssi <jessica.buehler@udo.edu>
2020-12-04 01:18:05 +0100

    prune

--- src/checkpoint_utils.py
+++ src/checkpoint_utils.py
@@ -339,12 +339,8 @@ def genDenseModel(model, dense_chs, optimizer, dataset):
 
         # Change parameters of non-neural computing layers (BN, biases)
         else:
-            i = int(name.split('.')[1])
-            j = None
-            if len(name.split('.')) == 4:
-                j = int(name.split('.')[2])
-            print("\n>Name: ", name)
-            w_name = (i,j)
+
+            w_name = name
             dense_out_ch_idxs = dense_chs[w_name]['out_chs']
             num_out_ch = len(dense_out_ch_idxs)
 
