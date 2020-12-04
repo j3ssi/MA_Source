ca6234d5ea22929f1d35250126ab78ff555462f3
j3ssi <jessica.buehler@udo.edu>
2020-12-04 21:49:26 +0100

    prune

--- src/checkpoint_utils.py
+++ src/checkpoint_utils.py
@@ -374,10 +374,14 @@ def genDenseModel(model, dense_chs, optimizer, dataset):
     # print(f'Change moving mean and var of BN')
     # Change moving_mean and moving_var of BN
     for name, buf in model.named_buffers():
+        i = int(name.split('.')[1])
+        j = None
+        if len(name.split('.')) == 4:
+            j = int(name.split('.')[2])
+
         print("\nBuffer Name: ", name)
         if 'running_mean' in name or 'running_var' in name:
-            i = int(name.split('.')[1])
-            w_name = 'module.conv' + str(int((i + 1) / 2)) + '.weight'
+            w_name = (i,j)
             # print("\nW_name2: ", w_name)
             dense_out_ch_idxs = dense_chs[w_name]['out_chs']
             num_out_ch = len(dense_out_ch_idxs)
