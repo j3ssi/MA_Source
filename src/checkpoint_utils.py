b2c6f462310e5b73ec9f29b89813f2c40c87212c
j3ssi <jessica.buehler@udo.edu>
2020-12-05 19:26:11 +0100

    prune

--- src/checkpoint_utils.py
+++ src/checkpoint_utils.py
@@ -276,7 +276,7 @@ def genDenseModel(model, dense_chs, optimizer):
         name = (i,j)
         # Get Momentum parameters to adjust
 
-        mom_param = optimizer.state_dict[k]['momentum_buffer']
+        mom_param = optimizer.state_dict()[k]['momentum_buffer']
         module = model.module_list[i]
         if j is not None:
             module = module[j]
