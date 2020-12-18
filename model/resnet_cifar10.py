bcb9864e2593eb2f05d7fa7dce8b16f6779112ac
j3ssi <jessica.buehler@udo.edu>
2020-12-18 03:39:43 +0100

    n2n

--- model/resnet_cifar10.py
+++ model/resnet_cifar10.py
@@ -63,7 +63,7 @@ class BasicBlock(nn.Module):
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
-        inPlanes = 16
+        inPlanes = 8
         self.in_planes = inPlanes
 
         self.features = [conv_bn(3, inPlanes, 1)]
